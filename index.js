f847730bcd11b963e229c04986142d486c4e9fd2
ankitSharma645 <145925577+ankitSharma645@users.noreply.github.com>
2024-06-15 22:39:01 +0530

    Update index.js
    v

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoDB = require('./db');
+const cors = require("cors");
+const path = require('path')
 mongoDB();
 
+app.use(cors());
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
     res.header(
@@ -19,6 +22,11 @@ app.use(express.json());
 app.use('/api',require("./Routes/CreateUser"));
 app.use('/api',require("./Routes/DisplayData"));
 app.use('/api',require("./Routes/OrderData"));
+app.use(express.static(path.join(__dirname, "./beta/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./beta/build/index.html"));
+});
 app.listen(8000,()=>{
     console.log(`Running  `)
 });
